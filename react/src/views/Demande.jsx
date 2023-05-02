ed3d4ebf6528a806c16076e24392d2913ca1044e
Anas-Kamoun <anaskamoun@gmail.com>
2023-05-02 10:08:45 +0100

    0205

--- react/src/views/Demande.jsx
+++ react/src/views/Demande.jsx
@@ -2,93 +2,159 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../Contexts/ContextProvider";
+import PageviewIcon from "@mui/icons-material/Pageview";
 
-export default function DemnadeUser(){
-    const [DCongee,setDCongee]= useState([]);
-    const [loading,setLoading]=useState(false);
-    const {user,setNotification}=useStateContext();
-
-    useEffect(()=>{
-        getDCongee();
-    },[])
-
-    const onDelete=(u)=>{
-        if(!window.confirm("Vous Voulez Annulez cette Demande")){
-            return
-        }
-        u.etat="Annulee";
-        axiosClient.put(`dconges/${u.id}`,u)
-        .then(()=>{
-            setNotification('Demnade Congee Annuler !')
-            getDCongee()
+export default function DemnadeUser() {
+  const [DCongee, setDCongee] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { user, setNotification } = useStateContext();
+  const [userValue, setUser] = useState({
+    name: "",
+  });
+  const [modal, setModal] = useState(false);
+  const toggleModal = (u) => {
+      axiosClient
+        .get(`/user/`)
+        .then(({ data }) => {
+          setUser(data.data);
+          console.log(data.data);
         })
-    }
-    const onValidate=(u)=>{
-      if(!window.confirm("Vous Voulez Validez cette Demande")){
-          return
-      }
-      u.etat="Validee";
-      axiosClient.put(`dconges/${u.id}`,u)
-      .then(()=>{
-          setNotification('Demnade Congee Validee !')
-          getDCongee()
-      })
+        .catch(() => {
+        });
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
   }
-    const getDCongee=()=>{
-        setLoading(true);
-        axiosClient.get(`/dconges`)
-        .then(({data})=>{
-            setLoading(false);
-            setDCongee(data.data)
-        })
-        .catch(()=>{
-            setLoading(false);
-        })
+
+  useEffect(() => {
+    getDCongee();
+  }, []);
+
+  const onDelete = (u) => {
+    if (!window.confirm("Vous Voulez Annulez cette Demande")) {
+      return;
     }
-    return(
-        <div>
-            <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-                <h1>Liste des demandes de Congés</h1>
-            </div>
-            <div className="card animated fadeInDown">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Type Congé</th>
-                            <th>Start Date</th>
-                            <th>End Date</th>
-                            <th>Etat</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    {loading && <tbody>
-                        <tr>
-                            <td colSpan="5" className="text-center">
-                            Loading...
-                            </td>
-                        </tr>
-                    </tbody>
-                    }
-                    {!loading && <tbody>
-                        {DCongee.map(u=>(
-                            <tr key={u.id}>
-                                <td>{u.id}</td>
-                                <td>{u.type}</td>
-                                <td>{u.type == 'Congee' && u.start_date}{u.type == 'autorisation' && u.start_autorisation}</td>
-                                <td>{u.type == 'Congee' && u.end_date}{u.type == 'autorisation' && u.start_autorisation+u.autorisation}</td>
-                                <td>{u.etat}</td>
-                                <td>
-                                    <button onClick={ev=>onValidate(u)}className="btn-add">Valider</button>
-                                    &nbsp;
-                                    <button onClick={ev=>onDelete(u)}className="btn-delete">Annuler</button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    }
-                </table>
-            </div>
-        </div>
-    )
+    u.etat = "Annulee";
+    axiosClient.put(`dconges/${u.id}`, u).then(() => {
+      setNotification("Demnade Congee Annuler !");
+      getDCongee();
+    });
+  };
+  const onValidate = (u) => {
+    if (!window.confirm("Vous Voulez Validez cette Demande")) {
+      return;
+    }
+    u.etat = "Validee";
+    axiosClient.put(`dconges/${u.id}`, u).then(() => {
+      setNotification("Demnade Congee Validee !");
+      getDCongee();
+    });
+  };
+  const getDCongee = () => {
+    setLoading(true);
+    axiosClient
+      .get(`/dconges`)
+      .then(({ data }) => {
+        setLoading(false);
+        setDCongee(data.data);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <h1>Liste des demandes de Congés</h1>
+      </div>
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Type Congé</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Etat</th>
+              <th>View</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {loading && (
+            <tbody>
+              <tr>
+                <td colSpan="5" className="text-center">
+                  Loading...
+                </td>
+              </tr>
+            </tbody>
+          )}
+          {!loading && (
+            <tbody>
+              {DCongee.map((u) => (
+                <tr key={u.id}>
+                  <td>{u.id}</td>
+                  <td>{u.type}</td>
+                  <td>
+                    {u.type == "Congee" && u.start_date}
+                    {u.type == "autorisation" && u.start_autorisation}
+                  </td>
+                  <td>
+                    {u.type == "Congee" && u.end_date}
+                    {u.type == "autorisation" &&
+                      u.start_autorisation + u.autorisation}
+                  </td>
+                  <td>{u.etat}</td>
+                  <td>
+                    <PageviewIcon onClick={(ev) => toggleModal(u)} />
+                    {modal && (
+                      <div className="modal">
+                        <div onClick={toggleModal} className="overlay"></div>
+                        <div className="modal-content">
+                          <h2>Detail de la demande</h2>
+                          &nbsp;
+                          <tr>
+                            <td><h3>Type de la demande : </h3></td>
+                            <td><h4>{u.type}</h4></td>
+                            <td><h3>nom : </h3></td>
+                            <td><h4>{userValue.name}</h4></td> 
+                          </tr>
+                          
+                          <button className="close-modal" onClick={toggleModal}>
+                            CLOSE
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </td>
+                  <td>
+                    <button onClick={(ev) => onValidate(u)} className="btn-add">
+                      Valider
+                    </button>
+                    &nbsp;
+                    <button
+                      onClick={(ev) => onDelete(u)}
+                      className="btn-delete"
+                    >
+                      Annuler
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
 }
