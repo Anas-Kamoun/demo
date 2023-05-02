38dec5e6f3c715bd4291d600a8920fc899f33422
Mehdii19 <mehdizaghmi0@gmail.com>
2023-05-03 00:18:35 +0100

    03/05

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,115 +1,208 @@
-import React, { useState } from "react";
-import "./css/style.css";
-import { useStateContext } from "../Contexts/ContextProvider";
+import {TeamOutlined ,SyncOutlined ,CheckCircleOutlined,PlusCircleOutlined } from "@ant-design/icons";
+import { Card, Space, Statistic, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
 
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend,} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-const LeaveRequestForm = () => {
-  const [name, setName] = useState("");
-  const [balance, setBalance] = useState(0);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-  const [leaveType, setLeaveType] = useState("");
-  const [reason, setReason] = useState("");
-  const [attachment, setAttachment] = useState(null);
-  const {user,setNotification}=useStateContext()
+ChartJS.register(CategoryScale,LinearScale,BarElement,Title,Tooltip,Legend);
 
+function Dashboard() {
+  const [orders, setOrders] = useState(0);
+  const [inventory, setInventory] = useState(0);
+  const [customers, setCustomers] = useState(0);
+  const [revenue, setRevenue] = useState(0);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Submit form data to server or perform other actions here
+  useEffect(() => {
+    // getOrders().then((res) => {
+    //   setOrders(res.total);
+    //   setRevenue(res.discountedTotal);
+    // });
+    // getInventory().then((res) => {
+    //   setInventory(res.total);
+    // });
+    // getCustomers().then((res) => {
+    //   setCustomers(res.total);
+    // });
+  }, []);
+
+  return (
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Dashboard and statistics</Typography.Title>
+      <Space direction="horizontal">
+        <DashboardCard
+          icon={
+            <TeamOutlined 
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,255,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Users"}
+          value={orders}
+        />
+        <DashboardCard
+          icon={
+            <PlusCircleOutlined 
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(0,0,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Demande congés"}
+          value={inventory}
+        />
+        <DashboardCard
+          icon={
+            <CheckCircleOutlined 
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(0,255,255,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Congés acceptés"}
+          value={customers}
+        />
+        <DashboardCard
+          icon={
+            <SyncOutlined 
+              style={{
+                color: "blue",
+                backgroundColor: "rgba(25,10,0,0.25)",
+                borderRadius: 20,
+                fontSize: 24,
+                padding: 8,
+              }}
+            />
+          }
+          title={"Congés en cours"}
+          value={revenue}
+        />
+      </Space>
+      <Space>
+        <RecentOrders />
+        <DashboardChart />
+      </Space>
+    </Space>
+  );
+}
+
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+function RecentOrders() {
+  const [dataSource, setDataSource] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    // getOrders().then((res) => {
+    //   setDataSource(res.products.splice(0, 3));
+    //   setLoading(false);
+    // });
+  }, []);
+
+  return (
+    <>
+      <Typography.Text>Recent Orders</Typography.Text>
+      <Table
+        columns={[
+          {
+            title: "Nom",
+            dataIndex: "nom",
+          },
+          {
+            title: "Email",
+            dataIndex: "email",
+          },
+          {
+            title: "Date",
+            dataIndex: "date",
+          },
+          {
+            title: "Solde",
+            dataIndex: "solde",
+          },
+        ]}
+        loading={loading}
+        dataSource={dataSource}
+        pagination={false}
+      ></Table>
+    </>
+  );
+}
+
+function DashboardChart() {
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    // getRevenue().then((res) => {
+    //   const labels = res.carts.map((cart) => {
+    //     return `User-${cart.userId}`;
+    //   });
+    //   const data = res.carts.map((cart) => {
+    //     return cart.discountedTotal;
+    //   });
+
+    //   const dataSource = {
+    //     labels,
+    //     datasets: [
+    //       {
+    //         label: "Revenue",
+    //         data: data,
+    //         backgroundColor: "rgba(255, 0, 0, 1)",
+    //       },
+    //     ],
+    //   };
+
+    //   setReveneuData(dataSource);
+    // });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Total des demandes acceptés ",
+      },
+    },
   };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Nom</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="balance">Solde de jours de congés restants</label>
-          <input
-            type="number"
-            id="balance"
-            value={balance}
-            onChange={(event) => setBalance(event.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="startDate">Date début</label>
-          <input
-            type="date"
-            id="startDate"
-            value={startDate}
-            onChange={(event) => setStartDate(event.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="endDate">Date fin</label>
-          <input
-            type="date"
-            id="endDate"
-            value={endDate}
-            onChange={(event) => setEndDate(event.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="leaveType">Nature du congé</label>
-          <select
-            id="leaveType"
-            value={leaveType}
-            onChange={(event) => setLeaveType(event.target.value)}
-            required
-          >
-            <option value="">--Choisissez une option--</option>
-            <option value="congé annuel">Congé annuel</option>
-            <option value="congé de maladie">Congé de maladie</option>
-            <option value="congé sans solde">Congé sans solde</option>
-            <option value="congé sans solde">Congé maternité </option>
-
-          </select>
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="reason">Raison de congé</label>
-          <textarea
-            id="reason"
-            value={reason}
-            onChange={(event) => setReason(event.target.value)}
-            required
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="attachment">Pièce jointe</label>
-          <input
-            type="file"
-            id="attachment"
-            onChange={(event) => setAttachment(event.target.files[0])}
-          />
-        </div>
-
-        <button type="submit" className="submit-button">
-          Submit
-        </button>
-      </form>
-    </div>
+    <Card style={{ width: 500, height: 250 }}>
+      <Bar options={options} data={reveneuData} />
+    </Card>
+    
   );
-};
+  
+}
 
-export default LeaveRequestForm;
 
+export default Dashboard;
 
