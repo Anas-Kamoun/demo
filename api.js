a359301d5b22be8f4bdf6cf3cac95870af5d0c6b
Anas-Kamoun <anaskamoun@gmail.com>
2023-05-25 13:27:22 +0100

    finale

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import axiosClient from './axios';
+import axios from "axios";
+import axiosClient from "./axios";
 
 const api = axiosClient.create({
-  baseURL: `http://localhost:8000/api`, // Remplacez avec votre URL réelle
+  baseURL: `https://back-end-grh.alwaysdata.net/api`, // Remplacez avec votre URL réelle
 });
 
 export default api;
